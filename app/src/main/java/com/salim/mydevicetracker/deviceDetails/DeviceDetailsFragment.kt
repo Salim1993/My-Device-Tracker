6df72f79352130a4bd0dcb52bd6dc4342c84c835
Salim1993 <salimbenkhaled93@gmail.com>
2022-01-26 17:36:11 -0700

    change settings, added Devices Details page

--- app/src/main/java/com/salim/mydevicetracker/deviceDetails/DeviceDetailsFragment.kt
+++ app/src/main/java/com/salim/mydevicetracker/deviceDetails/DeviceDetailsFragment.kt
@@ -7,13 +7,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.navArgs
 import com.salim.mydevicetracker.R
 import com.salim.mydevicetracker.databinding.DeviceDetailsFragmentBinding
+import timber.log.Timber
 
 class DeviceDetailsFragment : Fragment(R.layout.device_details_fragment) {
 
     private val viewModel: DeviceDetailsViewModel by viewModels()
     private lateinit var binding: DeviceDetailsFragmentBinding
+    private val args: DeviceDetailsFragmentArgs by navArgs()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,6 +28,24 @@ class DeviceDetailsFragment : Fragment(R.layout.device_details_fragment) {
     }
 
     private fun setupObservers() {
-        //viewModel.loadDeviceDetailsState()
+        viewModel.liveData.observe(viewLifecycleOwner) {
+            binding.costText.text = String.format(getString(R.string.price), it.price, it.currency)
+            binding.typeText.text = String.format(getString(R.string.type), it.type)
+            binding.isOnlineText.text = if(it.isOnline)
+                getString(R.string.online_status)
+            else
+                getString(R.string.offline_status)
+            binding.isFavoritedText.text = if(it.isFavorite)
+                getString(R.string.is_favorite)
+            else
+                getString(R.string.not_favorite)
+
+            binding.descriptionText.text = if(it.description.isEmpty())
+                "NA"
+            else
+                it.description
+        }
+
+        viewModel.loadDeviceDetailsState(args.device)
     }
 }
--- app/src/main/java/com/salim/mydevicetracker/deviceDetails/DeviceDetailsViewModel.kt
+++ app/src/main/java/com/salim/mydevicetracker/deviceDetails/DeviceDetailsViewModel.kt
@@ -1,19 +1,24 @@
 package com.salim.mydevicetracker.deviceDetails
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.salim.mydevicetracker.devices.Devices
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class DeviceDetailsViewModel : ViewModel() {
 
-    private val deviceDetailsStateFlow = MutableSharedFlow<Devices>()
+    //Had to use Live data instead of shared flow, since got into weird issue where
+    // flow wouldn't emit new data
+    private val _liveData = MutableLiveData<Devices>()
+    val liveData: LiveData<Devices>
+        get() = _liveData
 
-    fun loadDeviceDetailsState(device: Devices): MutableSharedFlow<Devices> {
-        viewModelScope.launch {
-            deviceDetailsStateFlow.emit(device)
-        }
-        return deviceDetailsStateFlow
+    fun loadDeviceDetailsState(device: Devices) {
+        _liveData.postValue(device)
     }
 }
